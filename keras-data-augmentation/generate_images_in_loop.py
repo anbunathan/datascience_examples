9128072349a2ed010d79adc476dc1e0e766cf14c
anbunathan <anbunathan.r@gmail.com>
2021-01-02 23:42:36 +0530

    Initial Repository

---
+++ keras-data-augmentation/generate_images_in_loop.py
@@ -0,0 +1,51 @@
+# USAGE
+# python generate_images.py --image dog.jpg --output generated_dataset/dogs
+# python generate_images.py --image cat.jpg --output generated_dataset/cats
+
+# import the necessary packages
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+import numpy as np
+import argparse
+import os
+
+directory = "input_images"
+out_dir = "generate_images"
+for filename in os.listdir(directory):
+	if filename.endswith(".png") or filename.endswith(".jpg"):
+		file = os.path.splitext(filename)[0]
+		outdir = out_dir + "/" + file
+		if not os.path.exists(outdir):
+			os.makedirs(outdir)
+		print(os.path.join(directory, filename))
+		print(os.path.splitext(filename)[0])
+		print("[INFO] loading example image...")
+		image = load_img(os.path.join(directory, filename))
+		image = img_to_array(image)
+		image = np.expand_dims(image, axis=0)
+		# construct the image generator for data augmentation then
+		# initialize the total number of images generated thus far
+		aug = ImageDataGenerator(
+			rotation_range=30,
+			zoom_range=0.15,
+			width_shift_range=0.2,
+			height_shift_range=0.2,
+			shear_range=0.15,
+			horizontal_flip=True,
+			fill_mode="nearest")
+		total = 0
+		# construct the actual Python generator
+		print("[INFO] generating images...")
+		imageGen = aug.flow(image, batch_size=1, save_to_dir=outdir,
+							save_prefix="image", save_format="jpg")
+		# loop over examples from our image data augmentation generator
+		for image in imageGen:
+			# increment our counter
+			total += 1
+			# if we have reached the specified number of examples, break
+			# from the loop
+			if total == 500:
+				break
+
+
